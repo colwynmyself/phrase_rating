5e259af340120d3ea73d51e7c5823dc11e1c9c6f
Colwyn Fritze-Moor <colwyn@colwyn.me>
2019-03-30 22:49:17 -0700

    api: add some types to the response

--- frontend/src/ts/util/api.ts
+++ frontend/src/ts/util/api.ts
@@ -3,16 +3,31 @@ import { Phrase } from '../classes/phrase';
 
 const BASE_URL: string = process.env.BASE_URL || 'http://localhost:8080';
 
+interface PhraseItem {
+    name: string;
+    humor: number;
+    severity: number;
+    importance: number;
+}
+
+interface PhraseResponse {
+    phrases: PhraseItem[];
+}
+
 async function request(path, method='GET') {
-    const url: string = `${BASE_URL}${path}`;
-    const res: AxiosResponse = await axios.request({
-        url,
-        method,
-    });
-    return res.data;
+    try {
+        const url: string = `${BASE_URL}${path}`;
+        const res: AxiosResponse = await axios.request({
+            url,
+            method,
+        });
+        return res.data;
+    } catch (error) {
+        alert(error);
+    }
 }
 
 export async function getPhrases() {
-    const data = await request('/phrases');
+    const data: PhraseResponse = await request('/phrases');
     return data.phrases.map(p => new Phrase(p.name, p.humor, p.severity, p.importance));
 }
